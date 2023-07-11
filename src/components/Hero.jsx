8b8ded63b8b6c7f5d53ad6ae4fe9fbc551fc77c8
neilsancheti <neilsancheti1@gmail.com>
2023-07-11 11:58:23 +0530

    Your commit message

---
+++ src/components/Hero.jsx
@@ -0,0 +1,52 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+  return (
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[150px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-1'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Neil</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am a Software developer,<br className='sm:block hidden' />
+            I develop 3D Visuals and <br className='sm:block hidden' /> Web Applications.
+          </p>
+          <p className=" my-8 text-sm">[Move the computer <br className='sm:block hidden' /> for the 3D effect!!]</p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[25px] h-[44px] rounded-2xl border-2 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-1 h-1 rounded-full bg-secondary mb-2'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,251 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    logo,
+  } from "../assets";
+  
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Dot Net Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Android Developer",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    /*{
+      name: "Redux Toolkit",
+      icon: redux,
+    },*/
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Software Developer",
+      company_name: "Bitmapper Integration Technologies",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "Feb 2023 - Present",
+      points: [
+        "Deployed Full Stack applications(GUI) using the C#.NET framework and QT-6 to create robust and scalable software solutions.",
+        "Implemented TCP/IP and Socket programming for efficient communication between network devices and applications.",
+        "Utilised UART protocols for data transmission to ensure secure and error-free data transfer.",
+        "Practised Object Oriented Programming (OOPS) concepts for better code organization and maintainability.",
+      ],
+    },
+    {
+      title: "Co-Founder",
+      company_name: "Moto Memories",
+      icon: logo,
+      iconBg: "#383E56",
+      date: "Aug 2022 - Present",
+      points: [
+        "Track Designing: Specializing in professional track designing services for both on-road and off-road events, ensuring optimal performance and safety for participants.",
+        "Corporate Team Building: Creating engaging and adrenaline-pumping experiences for corporate team building activities, including car and motorcycle rallies, fostering teamwork and camaraderie.",
+        "Off-Site Events: Offering unique and thrilling off-site corporate event experiences that go beyond traditional venues, providing memorable moments for clients and their teams.",
+        "Customized Engagements: Tailoring packages to meet specific client needs, delivering exceptional automotive event services, and ensuring customer satisfaction every step of the way.",
+      ],
+    },
+    
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as this, but Neil proved me wrong.",
+      name: "Saket Sabane",
+      designation: "Software Developer",
+      company: "TCS",
+      image: "https://randomuser.me/api/portraits/men/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Neil does.",
+      name: "Sairohit Choudhary",
+      designation: "Business Analyst",
+      company: "Avis Budget Grp.",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Neil optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Rahul Sancheti",
+      designation: "Founder",
+      company: "Moto Dreamz",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "MyHealthPal",
+      description:
+        "Web-based platform that allows users to customise and plan, AI-backed workout and diet plans.",
+      tags: [
+        {
+          name: "Flask",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "OpenAI API",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/neilsancheti/MyHealthPal",
+    },
+    /*
+    {
+      name: "Portfolio Neil Sancheti",
+      description:
+        "Developed an intuitive website to showcase my projects, and work, and a brief about me",
+      tags: [
+        {
+          name: "Three JS",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "React",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Three Fibre",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "Framer Motion",
+          color: "blue-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/neilsancheti/neil_portfolio.github.io",
+    },
+    */
+    {
+      name: "Weather App",
+      description:
+        "An Android application that displays the weather forecast of specified location.",
+      tags: [
+        {
+          name: "Android",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Material Design 2",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Open Weather API",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/neilsancheti",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
